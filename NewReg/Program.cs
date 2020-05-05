a13083de1f742bdab87338d43aab1d78207f98c0
alwaysresist <44613719+alwaysresist@users.noreply.github.com>
2020-05-05 16:19:50 +0300

    1

---
+++ NewReg/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NewReg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] s = { "127.0.0.1", "abd.def.gha.bcd", "170.1.1.1.1", "255.255.255.254", "142.90.39.124" };
+            //string[] s = { "e02fd0e4-00fd-090A-ca30-0d00a0038ba0", "e02fd0e4.00fd.090A.ca30.0d00a0038ba0", "e02fd0e4fasfafds2340d00a0038ba0", "22345200-abe8-4f60-90c8-0d43c5f6c0f6", "6F9619FF-8B86-D011-B42D-00CF4FC964FF" };
+            //string[] s = { "http://a.com", "http://adasd.com", "http://www.example.com", "text.ru", "http://google.ua" };
+            string[] s = { "26/11/1737", "16.09.1814", "09/03/1965", "29/02/2000", "13-12-1729","29/02/1598" };
+
+            foreach (string elem in s)
+            {
+                if (elem.IsDate())
+                    Console.WriteLine(elem + " - правильное выражение.\n");
+                else
+                    Console.WriteLine(elem + " - неправильное выражение.\n");
+            }
+        }
+    }
+}
---
+++ NewReg/RegularExpressions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace NewReg
+{
+    public static class RegularExpressions
+    {
+        public static bool IsIP(this string s)
+        {
+            Regex reg = new Regex(@"^(((0)|([1-9]\d?)|(1\d\d)|(2[0-5]{2}))\.?){0,3}((0)|([1-9]\d?)|(1\d\d)|(2[0-5]{2}))?$");
+            return reg.IsMatch(s);
+        }
+
+        public static bool IsGUID(this string s)
+        {
+            Regex reg = new Regex(@"^[{(]?[0-9a-fA-F]{8}-([0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12}[)}]?$");
+            return reg.IsMatch(s);
+        }
+
+        public static bool IsURL(this string s)
+        {
+            Regex reg = new Regex(@"^\b(?:http(?:s)?:\/\/)?(?:www\.)?(?:[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9]\.)+(?:[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])(?::\d+)?(?:\/\w+(?:\.\w+)?)*((\/)?\?\w+=\w+(?:&\w+=\w+)*)?(\/)?(#\w+)?$");
+            return reg.IsMatch(s);
+        }
+
+        public static bool IsDate(this string s)
+        {
+            Regex reg = new Regex(@"^([012][1-9]|10|20|30|31)/(01|03|05|07|08|10|12)/((1[6-9]|[2-9][0-9])[0-9]{2})|([012][1-9]|10|20|30)/(04|06|09|11)/((1[6-9]|[2-9][0-9])[0-9]{2})|29/02/((1[6-9]|[2-9][0-9])(04|08|[13579][26]|[2468][480])|(16|[2468][048]|[3579][26])00)|(0[1-9]|1[0-9]|2[0-8])/02/((1[6-9]|[2-9][0-9])[0-9]{2})$");
+            return reg.IsMatch(s);
+        }
+        public static bool IsPrice(this string s)
+        {
+            Regex reg = new Regex(@"^\d{1,}(\.\d{0,2})? ?р(уб)?\.?$");
+            return reg.IsMatch(s);
+        }
+
+        public static bool IsValidPass(this string s)
+        {
+            Regex reg = new Regex(@"((?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?!.* ).{8,})");
+            return reg.IsMatch(s);
+        }
+    }
+}
